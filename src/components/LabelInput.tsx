b786a9893fd1c68a091dfb5d41aafa72d3ed7dad
elianbenjamin <elianbenjamin30@gmail.com>
2023-12-19 01:22:19 -0300

    tailwind css

--- src/components/LabelInput.tsx
+++ src/components/LabelInput.tsx
@@ -21,12 +21,19 @@ const LabelInput: FC<LabelInputProps> = ({
         {labelText}
       </label>
       {textarea ? (
-<textarea id={placeholderText} rows={9} placeholder="Message" className="bg-accent rounded-2xl py-4 px-6 text-textPrimary text-base sm:text-lg placeholder-textSecondary outline-none resize-none border-[1px] border-transparent focus:border-hoverSecondary">
-
-</textarea>
-      ):(
-        <input type="text" id={placeholderText} autoComplete="off"  />
-
+        <textarea
+          id={placeholderText}
+          rows={9}
+          placeholder="Message"
+          className="bg-accent rounded-2xl py-4 px-6 text-textPrimary text-base sm:text-lg placeholder-textSecondary outline-none resize-none border-[1px] border-transparent focus:border-hoverSecondary"
+        ></textarea>
+      ) : (
+        <input
+          type="text"
+          id={placeholderText}
+          autoComplete="off"
+          className="bg-accent rounded-2xl py-4 px-6 text-textPrimary text-base sm:text-lg placeholder-textSecondary outline-none resize-none border-[1px] border-transparent focus:border-hoverSecondary"
+        />
       )}
     </div>
   );
