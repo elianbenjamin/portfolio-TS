14ee6dc48177dd749c4d78596d3e7593396e2cbc
elianbenjamin <elianbenjamin30@gmail.com>
2023-12-22 19:32:37 -0300

    css

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -12,7 +12,7 @@ import { Link } from "react-scroll";
 
 const Menu = () => {
   return (
-    <div className="fixed bottom-0 sm:bottom-12 w-full sm:w-fit left-1/2 -translate-x-1/2 bg-accent rounded-t-xl sm:rounded-full py-3 px-6 items-center justify-around sm:justify-center gap-12 z-30">
+    <div className="fixed bottom-0 sm:bottom-12 w-full sm:w-fit left-1/2 -translate-x-1/2 bg-accent rounded-t-xl sm:rounded-full py-3 px-6 items-center justify-around sm:justify-center gap-12 z-30 sm:mx-auto">
 
       <Link to="home" smooth>
         <Tooltip title="Home" placement="top" arrow>
